7ec9d469c5bae82062c420fae6202ebf9fff02a8
Alma Qasreena Morsidi <almaqasreena@gmail.com>
2024-05-12 01:08:42 +0200

    Added an image for IndexSite

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
 const express = require("express");
+const logger = require("./utils/logger");
+const handlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
+
 const dotenv = require("dotenv");
 
 /* Reading global variables from config file */
 dotenv.config();
 const PORT = process.env.PORT;
 
-/*
- *
- * Express setup
- *
-*/
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.engine('.hbs', handlebars.engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', './views');
 
-app = express();
+const routes = require("./routes");
+app.use("/", routes);
+
 
 //turn on serving static files (required for delivering css to client)
 app.use(express.static("public"));
 //configure template engine
-app.set("views", "views");
-app.set("view engine", "pug");
+//app.set("views", "views");
+//app.set("view engine", "pug");
 
 app.get('/', (req, res) => {
     res.send("Hello weathertop!");
@@ -26,3 +32,5 @@ app.get('/', (req, res) => {
 app.listen(PORT, function() {
   console.log(`Weathertop running and listening on port ${PORT}`);
 });
+
+module.exports = app;
